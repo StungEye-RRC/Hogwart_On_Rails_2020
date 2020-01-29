efedeffba4260110d125baebcbfbc77a7edd3e78
Kyle Geske <stungeye@gmail.com>
2020-01-29 17:07:14 -0600

    Removed frozen string comments from models.

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class House < ApplicationRecord
   has_many :students
 
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Student < ApplicationRecord
   belongs_to :house
 
