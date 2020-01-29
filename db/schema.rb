9e19e84c0349280ff8817c6b460e4f1a41f5e7df
Kyle Geske <stungeye@gmail.com>
2020-01-29 16:18:11 -0600

    After running the students migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_212944) do
+ActiveRecord::Schema.define(version: 2020_01_29_215241) do
 
   create_table "houses", force: :cascade do |t|
     t.string "name"
@@ -19,4 +19,13 @@ ActiveRecord::Schema.define(version: 2020_01_29_212944) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "house_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["house_id"], name: "index_students_on_house_id"
+  end
+
+  add_foreign_key "students", "houses"
 end
