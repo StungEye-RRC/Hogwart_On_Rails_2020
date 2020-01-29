79e7eaea10bb60498392ad804754392715851b1d
Kyle Geske <stungeye@gmail.com>
2020-01-29 16:21:47 -0600

    Generating students now for all the houses.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
+Student.destroy_all
 House.destroy_all
 
-8.times do
+5.times do
   house_name = Faker::Movies::HarryPotter.unique.house
   house_points = rand(1..1234)
-  House.create(name: house_name, points: house_points)
+  house = House.create(name: house_name, points: house_points)
+
+  students_in_house = rand(5..10)
+  students_in_house.times do
+    house.students.create(name: Faker::Movies::HarryPotter.unique.character)
+  end
 end
 
 puts "Created #{House.count} Houses."
+puts "Created #{Student.count} Students."
