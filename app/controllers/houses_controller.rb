c7ab7926be67cd0a69bcd7909475b3a2e7d687cd
Kyle Geske <stungeye@gmail.com>
2020-01-29 17:07:42 -0600

    Implemented the index and show house controller actions and views.

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,7 +1,17 @@
 class HousesController < ApplicationController
+  # GET /
   def index
+    @houses = House.all
   end
+  # Then automagically load the associated view:
+  #   app/views/houses/index.html.erb
+  # And share the @houses instance var with this view.
 
+  # GET /houses/:id
   def show
+    @house = House.find(params[:id])
   end
+  # Then automagically load the associated view:
+  #   app/views/houses/show.html.erb
+  # And share the @house instance var with this view.
 end
