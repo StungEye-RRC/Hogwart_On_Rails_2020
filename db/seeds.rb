a6c61cf3b0d0ebc7fe53c4f658d46f4c8736d2dc
Kyle Geske <stungeye@gmail.com>
2020-01-29 15:55:13 -0600

    Creating 8 houses using Faker in the seed script.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+House.destroy_all
+
+8.times do
+  house_name = Faker::Movies::HarryPotter.unique.house
+  house_points = rand(1..1234)
+  House.create(name: house_name, points: house_points)
+end
+
+puts "Created #{House.count} Houses."
